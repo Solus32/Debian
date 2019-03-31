0b51a58461a84ae9fff6cd11c6e630e419e60981
José Luis <Solus32@users.noreply.github.com>
2019-03-31 12:48:55 -0600

    Add files via upload

--- Limpiar Sistema Icono/limpieza-automatica.sh
+++ Limpiar Sistema Icono/limpieza-automatica.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 echo "\e[93mSe Limpiara el Sistema, de las Actualizaciones Remanentes"
 echo
-echo "\e[91mTeclea tu Contraseña"
+echo "\e[93mTerminada la Limpieza se Cerrará la Ventana"
+echo "\e[0m"
+echo "\e[91mSe necesita permisos Administrativos. Teclea tu Contraseña."
 echo "\e[0m"
 sudo apt clean
 sudo apt autoclean
