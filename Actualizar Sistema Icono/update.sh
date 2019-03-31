f55a2ea20c126b56d107220aefdb41b529a532f6
José Luis <Solus32@users.noreply.github.com>
2019-03-31 12:49:11 -0600

    Add files via upload

--- Actualizar Sistema Icono/update.sh
+++ Actualizar Sistema Icono/update.sh
@@ -2,7 +2,7 @@
 echo "\e[93mSe Descargarán las Actualizaciones, y se Instalarán"
 echo
 echo "\e[93mTerminada la Instalación se Cerrará la Ventana"
-echo
+echo "\e[0m"
 echo "\e[91mSe necesita permisos Administrativos. Teclea tu Contraseña."
 echo "\e[0m"
 sudo apt update
